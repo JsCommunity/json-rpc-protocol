66864dea1e3cfe18fc6ea68faf74f10761662dbd
Julien Fontanet <julien.fontanet@isonoe.net>
2023-11-30 10:33:29 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import * as format from './format'
+import { parse } from './parse'
 
 export * from './json-rpc.type'
 export * from './errors'
 export { parse } from './parse'
 export { format }
+
+export function parseResponseResult (message: string | object) {
+  const parsedMessage = parse(message)
+  const { type } = parsedMessage
+  if (type === 'error') {
+    throw parsedMessage.error
+  }
+  if (type !== 'response') {
+    throw new Error(`expected a response, instead got a ${type}`)
+  }
+  return parsedMessage.result
+}
